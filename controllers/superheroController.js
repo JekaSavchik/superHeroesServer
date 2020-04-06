0b629985e2197e547a22f122005ab0255413c097
Evgeniy <savchik.jeka@gmail.com>
2020-04-06 18:34:13 +0300

    coreckt getSuperheroes

--- controllers/superheroController.js
+++ controllers/superheroController.js
@@ -10,10 +10,16 @@ exports.getSuperheroes = function (request, response) {
             console.log(err);
             return response.sendStatus(400);
         }
+        let superheroes = [];
+        allSuperheroes.forEach((item) => {
+            superheroes.push({
+                id: item._id,
+                nickName: item.nickName,
+                image: item.images[0]
+            });
+        });
         response.render("superheroes.hbs", {
-            superheroes: allSuperheroes.map(hero => hero.toJSON({
-                virtuals: true
-            }))
+            superheroes: superheroes
         });
     });
 }
@@ -26,10 +32,9 @@ exports.getSuperheroId = function (request, response) {
         if (err) {
             return response.sendStatus(400);
         }
-        if (request._parsedOriginalUrl.path.match(/edit/i)){
+        if (request._parsedOriginalUrl.path.match(/edit/i)) {
             response.render("edit.hbs", superhero);
-        }
-        else{
+        } else {
             response.render("superhero.hbs", superhero);
         }
     });
@@ -72,16 +77,6 @@ exports.postSuperhero = function (request, response) {
         catchPhrase: heroCatchPhrase,
         images: heroImages
     });
-
-    //         const superhero = new Superhero({
-    //     nickName: "Spider-Man",
-    //     realName: "Piter Parker",
-    //     originDescription: "an unusual spider bit him",
-    //     superpowers: "​Spider-Man received superpower, increased dexterity, “spider instinct”, as well as the ability to stay on steep surfaces and release cobwebs from his hands using a device of his own invention",
-    //     catchPhrase: "Friendly neighbor spiderman",
-    //     images: ["/images/spider_man.jpg", "/images/231px-AmazingSpiderMan50.jpg"]
-    // });
-
 
     superhero.save(function (err) {
         if (err) {
